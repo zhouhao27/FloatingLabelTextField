c4c294ff44b7ea398bb05ba5d07810d13c1e9319
Zhou Hao <hao.zhou@cognizant.com>
2019-09-12 01:38:39 +0800

    Replace Divider with Rectangle

--- Sources/FloatingLabelTextField/FloatingLabelTextField.swift
+++ Sources/FloatingLabelTextField/FloatingLabelTextField.swift
@@ -30,6 +30,8 @@ public struct FloatingLabelTextField: View {
   }
   
   public var body: some View {
+    let lineHeight: CGFloat = self.isActive ? 2 : 1
+    let lineColor = self.isActive ? Color.blue : Color.gray
     
     return VStack(alignment: .leading) {
       Text(placeHolderLabel).font(.footnote).foregroundColor(.gray)
@@ -39,7 +41,14 @@ public struct FloatingLabelTextField: View {
         self.placeHolder = self.placeHolderValue
       }
 
-      Divider().background(self.isActive ? Color.blue : Color.gray)
+//      Divider()
+//        .background(self.isActive ? Color.blue : Color.gray)
+//        .frame(minHeight: 3, idealHeight: 5, maxHeight: 5, alignment: .center)
+      
+      Rectangle()
+        .fill(lineColor)
+        .frame(minHeight: lineHeight, idealHeight: lineHeight, maxHeight: lineHeight, alignment: .center)
+      
     }
     .padding(.horizontal,20)
     .padding(.vertical, 20)
